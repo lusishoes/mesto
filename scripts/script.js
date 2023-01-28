11fdbc52985f44c1e5a129a237ed8a3223a1d2ec
Yury Gavrilov <gavrilov.invest@mail.ru>
2023-01-28 14:23:45 +0800

    add javascript features

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
+'use strict'
+
 let popup = document.querySelector('.popup');
 let editBtn = document.querySelector('.profile__edit-button');
 let closeBtn = document.querySelector('.popup__close-icon');
 
 const handleAboutButtonClick = () => { 
     popup.classList.add('popup_opened');
+    formPopup.querySelector('.input__text_type_name').value = nameInput.textContent;
+    formPopup.querySelector('.input__text_type_occupation').value = occupationInput.textContent;
 }
 
 const handleCloseButtonClick = () => { 
@@ -12,28 +16,18 @@ const handleCloseButtonClick = () => {
 
 editBtn.addEventListener('click', handleAboutButtonClick);
 closeBtn.addEventListener('click', handleCloseButtonClick);
-/*Имя чела на странице*/
+
+let formPopup = document.querySelector('.popup__container') ;
 let nameInput = document.querySelector('.profile__title');
-/*деятельность чела на странице*/
 let occupationInput = document.querySelector('.profile__occupation');
-console.log(nameInput, occupationInput);
-/*Кнопка submit */
-let submitButton = document.querySelector('.popup__button');
 
-function takesmth(evt) {
-    evt.preventDefault();
-    /*имя чела через инпут */
-    let nameInputValue = document.querySelector('.input__text_type_name').value;
-    /*деятельность чела через инпут*/ 
-    let occupationInputValue = document.querySelector('.input__text_type_occupation').value;
-    if(((nameInputValue && occupationInputValue)!== '') || ((nameInputValue || occupationInputValue)!== '')) {
+formPopup.addEventListener('submit', function(e){
+    e.preventDefault();
+        let nameInputValue = formPopup.querySelector('.input__text_type_name').value;
+        let occupationInputValue = formPopup.querySelector('.input__text_type_occupation').value;
+    if(nameInputValue !== '' && occupationInputValue !== '') {
         nameInput.textContent = nameInputValue;
-        console.log(nameInput);
         occupationInput.textContent = occupationInputValue;
-        console.log(occupationInput);
     }
-    handleCloseButtonClick();
-}
-
-submitButton.addEventListener('click', takesmth);
-
+    handleCloseButtonClick();   
+});
